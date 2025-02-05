1ffef2c41b464cca9e025940419e11f6ac93a5b3
jeetpatel1113 <jmpatel7238@gmail.com>
2025-02-04 23:00:19 -0800

    Two sum

--- two_sum.py
+++ two_sum.py
@@ -1,1 +1,9 @@
-print (1)
+class Solution(object):
+    def twoSum(self, nums, target):
+        # Dictionary to store the indices of the numbers
+        index = {}
+        for i, num in enumerate(nums):
+            temp = target - num
+            if temp in index:
+                return [index[temp], i]
+            index[num] = i
